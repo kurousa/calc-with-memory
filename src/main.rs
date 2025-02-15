921408a8a7f82036f1a53469a62c493ffd214f0e
kurousa <r.takahashi.1026@gmail.com>
2025-02-15 23:31:57 +0900

    メモリ機能実装

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
-/// 電卓
+/// メモリ機能付き電卓
 ///
 /// 仕様
 ///
 /// - 計算式を1行ずつ読み込んで処理
 ///   - 空白区切りで、数値 演算子 数値といった入力をすることで計算結果を出力する
+///   - mem+、mem-とだけ入力すると、直前の計算結果がメモリに足し引きされる
+///   - 計算式の 数値 の部分が、 mem となっていた場合、数値代わりにメモリの値を利用する
 /// - 計算結果は整数型ではなく小数型(f64)で管理
 ///
 /// 例
@@ -13,6 +15,8 @@
 /// ``
 use std::io::stdin;
 fn main() {
+    let mut memory: f64 = 0.0;
+    let mut prev_result: f64 = 0.0;
     println!("Please input calculation formula like 1 + 2, 2 - 1, 3 * 4, 4 / 2");
     for line in stdin().lines() {
         let line = line.unwrap();
@@ -23,11 +27,24 @@ fn main() {
         }
         // 入力を空白区切りで分割
         let tokens: Vec<&str> = line.split(char::is_whitespace).collect();
-        // 数値と演算子(+-*/)を取得
-        // 所定の演算子出ない場合は異常終了する
-        let left: f64 = tokens[0].parse().unwrap();
-        let right: f64 = tokens[2].parse().unwrap();
-        let result: f64 = match tokens[1] {
+        let first_token = tokens[0];
+
+        // メモリ機能の処理
+        if first_token == "mem+" {
+            set_memory_print_value(&mut memory, prev_result); // memoryの参照渡し
+            continue;
+        } else if first_token == "mem-" {
+            set_memory_print_value(&mut memory, -prev_result); // memoryの参照渡し
+            continue;
+        }
+
+        let third_token = tokens[2];
+        // 数値部を取得
+        // memの場合はmemoryの値を利用
+        let left: f64 = eval_token(first_token, memory);
+        let right: f64 = eval_token(third_token, memory);
+        let expression: &str = tokens[1];
+        let result: f64 = match expression {
             "+" => add_value(left, right),
             "-" => sub_value(left, right),
             "*" => multiply_value(left, right),
@@ -35,13 +52,14 @@ fn main() {
             _ => unreachable!("Invalid operator, use only +, -, *, /"),
         };
         print_formula_result(line, result);
+        prev_result = result;
     }
 }
 
 // 計算結果出力
 fn print_formula_result(formula: String, result: f64) {
     println!("{} equal {}", formula, result);
-    }
+}
 
 // 加算処理
 fn add_value(left: f64, right: f64) -> f64 {
@@ -58,4 +76,17 @@ fn multiply_value(left: f64, right: f64) -> f64 {
 // 除算処理
 fn divide_value(left: f64, right: f64) -> f64 {
     left / right
+}
+
+fn eval_token(token: &str, memory: f64) -> f64 {
+    if token == "mem" {
+        memory
+    } else {
+        token.parse().unwrap()
+    }
+}
+
+fn set_memory_print_value(memory: &mut f64, prev_result: f64) {
+    *memory += prev_result;
+    print_formula_result("set memory".to_string(), *memory);
 }
