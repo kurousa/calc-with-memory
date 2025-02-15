097ba612e870376456afd59ea2a7ee4a65f27f66
kurousa <r.takahashi.1026@gmail.com>
2025-02-15 22:25:07 +0900

    単純な四則演算を処理する機能を実装

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
+/// 電卓
+///
+/// 仕様
+///
+/// - 計算式を1行ずつ読み込んで処理
+///   - 空白区切りで、数値 演算子 数値といった入力をすることで計算結果を出力する
+/// - 計算結果は整数型ではなく小数型(f64)で管理
+///
+/// 例
+/// ```rust
+/// 1 + 2
+/// => 3
+/// ``
+use std::io::stdin;
 fn main() {
-    println!("Hello, world!");
+    println!("Please input calculation formula like 1 + 2, 2 - 1, 3 * 4, 4 / 2");
+    for line in stdin().lines() {
+        let line = line.unwrap();
+        if line.is_empty() {
+            // 空行の場合処理を終了
+            break;
+        }
+        // 入力を空白区切りで分割
+        let tokens: Vec<&str> = line.split(char::is_whitespace).collect();
+        // 数値と演算子(+-*/)を取得
+        // 所定の演算子出ない場合は異常終了する
+        let left: f64 = tokens[0].parse().unwrap();
+        let right: f64 = tokens[2].parse().unwrap();
+        let result: f64 = match tokens[1] {
+            "+" => left + right,
+            "-" => left - right,
+            "*" => left * right,
+            "/" => left / right,
+            _ => panic!("Invalid operator"),
+        };
+        println!("{} equal {}", line, result);
+    }
 }
