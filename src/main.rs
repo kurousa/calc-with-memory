073de16901f917db6588a9dd36d31e5fd37be4fc
kurousa <r.takahashi.1026@gmail.com>
2025-02-15 23:52:10 +0900

    メモリ機能拡張WIP - 所有権の概念を理解するためにあえてコンパイルエラーの状態でコミット

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@
 ///   - 空白区切りで、数値 演算子 数値といった入力をすることで計算結果を出力する
 ///   - mem+、mem-とだけ入力すると、直前の計算結果がメモリに足し引きされる
 ///   - 計算式の 数値 の部分が、 mem となっていた場合、数値代わりにメモリの値を利用する
+///   - メモリは10個まで保持可能
 /// - 計算結果は整数型ではなく小数型(f64)で管理
 ///
 /// 例
@@ -15,7 +16,7 @@
 /// ``
 use std::io::stdin;
 fn main() {
-    let mut memory: f64 = 0.0;
+    let mut memories: Vec<f64> = vec![0.0; 10];
     let mut prev_result: f64 = 0.0;
     println!("Please input calculation formula like 1 + 2, 2 - 1, 3 * 4, 4 / 2");
     for line in stdin().lines() {
@@ -30,19 +31,20 @@ fn main() {
         let first_token = tokens[0];
 
         // メモリ機能の処理
-        if first_token == "mem+" {
-            set_memory_print_value(&mut memory, prev_result); // memoryの参照渡し
+        let is_memory = first_token.starts_with("mem");
+        if is_memory && first_token.ends_with('+') {
+            set_memory_print_value(&mut memories, first_token, prev_result);
             continue;
-        } else if first_token == "mem-" {
-            set_memory_print_value(&mut memory, -prev_result); // memoryの参照渡し
+        } else if is_memory && first_token.ends_with('-') {
+            set_memory_print_value(&mut memories, first_token, -prev_result); // memoryの参照渡し
             continue;
         }
 
         let third_token = tokens[2];
         // 数値部を取得
         // memの場合はmemoryの値を利用
-        let left: f64 = eval_token(first_token, memory);
-        let right: f64 = eval_token(third_token, memory);
+        let left: f64 = eval_token(first_token, memories);
+        let right: f64 = eval_token(third_token, memories);
         let expression: &str = tokens[1];
         let result: f64 = match expression {
             "+" => add_value(left, right),
@@ -78,15 +80,19 @@ fn divide_value(left: f64, right: f64) -> f64 {
     left / right
 }
 
-fn eval_token(token: &str, memory: f64) -> f64 {
-    if token == "mem" {
-        memory
+fn eval_token(token: &str, memories: Vec<f64>) -> f64 {
+    if token.starts_with("mem") {
+        let slot_index: usize = token[3..].parse().unwrap();
+        memories[slot_index]
     } else {
         token.parse().unwrap()
     }
 }
 
-fn set_memory_print_value(memory: &mut f64, prev_result: f64) {
-    *memory += prev_result;
-    print_formula_result("set memory".to_string(), *memory);
+fn set_memory_print_value(memories: &mut Vec<f64>, token: &str, prev_result: f64) {
+    // tokenの3文字目から最後の文字の1つ前までを取得
+    // 例: mem10+ といった文字列の場合、10を取得
+    let slot_index: usize = token[3..token.len() - 1].parse().unwrap();
+    memories[slot_index] += prev_result;
+    print_formula_result("set memory".to_string(), memories[slot_index]);
 }
