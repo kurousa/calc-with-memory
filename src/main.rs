dda972e58bf4817f654c0e3b9cf53a9a21040b46
kurousa <r.takahashi.1026@gmail.com>
2025-02-22 22:31:36 +0900

    feat: 名前付きメモリ保存の実装

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@
 ///   - mem+、mem-とだけ入力すると、直前の計算結果がメモリに足し引きされる
 ///   - 計算式の 数値 の部分が、 mem となっていた場合、数値代わりにメモリの値を利用する
 ///   - メモリは10個まで保持可能
+///   - メモリには名前付きで(例("名前",値)のタプルでの表現)が可能
 /// - 計算結果は整数型ではなく小数型(f64)で管理
 ///
 /// 例
@@ -15,8 +16,55 @@
 /// => 3
 /// ``
 use std::io::stdin;
+
+/// メモリ構造体
+struct Memory {
+    /// メモリの名前と値のタプルを配列で保持
+    slots: Vec<(String, f64)>,
+}
+/// Memory構造体に関わるメソッドの実装
+impl Memory {
+    /// Memory構造体の初期化
+    fn new() -> Self {
+        Self { slots: vec![] }
+    }
+    /// tokenがmemで始まる場合はメモリの値を返却
+    fn eval_token(&self, token: &str) -> f64 {
+        if token.starts_with("mem") {
+            let slot_name: &str = &token[3..];
+            for slot in &self.slots {
+                if slot.0 == slot_name {
+                    // 保管している値を返却
+                    return slot.1;
+                }
+            }
+            // メモリが見つからない場合は初期値0.0を返却
+            0.0
+        } else {
+            token.parse().unwrap()
+        }
+    }
+    /// メモリに値を設定し、コンソールに出力
+    fn set_memory_print_value(&mut self, token: &str, prev_result: f64) {
+        // tokenの3文字目から最後の文字の1つ前までを取得
+        // 例: mem10+ といった文字列の場合、10を取得
+        let slot_name: &str = &token[3..token.len() - 1];
+        for slot in self.slots.iter_mut() {
+            if slot.0 == slot_name {
+                // 既に存在する場合は値を更新し、コンソールに出力して終了
+                slot.1 += prev_result;
+                print_formula_result(format!("set memory{}", slot_name), slot.1);
+                return;
+            }
+        }
+        // メモリが見つからなかった場合は末尾に追加
+        self.slots.push((slot_name.to_string(), prev_result));
+        print_formula_result(format!("set memory{}", slot_name), prev_result);
+    }
+}
+
 fn main() {
-    let mut memories: Vec<f64> = vec![0.0; 10];
+    let mut memories: Memory = Memory::new();
     let mut prev_result: f64 = 0.0;
     println!("Please input calculation formula like 1 + 2, 2 - 1, 3 * 4, 4 / 2");
     for line in stdin().lines() {
@@ -33,18 +81,18 @@ fn main() {
         // メモリ機能の処理
         let is_memory = first_token.starts_with("mem");
         if is_memory && first_token.ends_with('+') {
-            set_memory_print_value(&mut memories, first_token, prev_result);
+            memories.set_memory_print_value(first_token, prev_result);
             continue;
         } else if is_memory && first_token.ends_with('-') {
-            set_memory_print_value(&mut memories, first_token, -prev_result); // memoryの参照渡し
+            memories.set_memory_print_value(first_token, -prev_result); // memoryの参照渡し
             continue;
         }
 
         let third_token = tokens[2];
         // 数値部を取得
         // memの場合はmemoryの値を利用
-        let left: f64 = eval_token(first_token, &memories);
-        let right: f64 = eval_token(third_token, &memories);
+        let left: f64 = memories.eval_token(first_token);
+        let right: f64 = memories.eval_token(third_token);
         let expression: &str = tokens[1];
         let result: f64 = match expression {
             "+" => add_value(left, right),
@@ -78,21 +126,4 @@ fn multiply_value(left: f64, right: f64) -> f64 {
 // 除算処理
 fn divide_value(left: f64, right: f64) -> f64 {
     left / right
-}
-
-fn eval_token(token: &str, memories: &[f64]) -> f64 {
-    if token.starts_with("mem") {
-        let slot_index: usize = token[3..].parse().unwrap();
-        memories[slot_index]
-    } else {
-        token.parse().unwrap()
-    }
-}
-
-fn set_memory_print_value(memories: &mut [f64], token: &str, prev_result: f64) {
-    // tokenの3文字目から最後の文字の1つ前までを取得
-    // 例: mem10+ といった文字列の場合、10を取得
-    let slot_index: usize = token[3..token.len() - 1].parse().unwrap();
-    memories[slot_index] += prev_result;
-    print_formula_result(format!("set memory{}", slot_index), memories[slot_index]);
 }
